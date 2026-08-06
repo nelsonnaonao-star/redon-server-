380300a93c03bfdd34aea0ccf390f00efe1fac95
Nelson <nelsonnaonao@gmail.com>
2026-08-06 14:57:15 -0400

    fix: delete message for everyone fails on NOT NULL text constraint

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -394,7 +394,7 @@ router.post('/delete', async (req, res) => {
 
     const { error } = await supabaseAdmin
       .from('messages')
-      .update({ is_deleted: true, text: null })
+      .update({ is_deleted: true, text: '' })
       .eq('id', message_id);
 
     if (error) throw error;
